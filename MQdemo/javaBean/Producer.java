3044fcccd5ef99366a216f21a9379c7041dbdb66
lyxshike <543421638@qq.com>
2018-10-16 12:51:22 +0800

    javabean consmuer

---
+++ MQdemo/javaBean/Producer.java
@@ -0,0 +1,47 @@
+public class Producer{
+     // ActiveMQ 的默认用户名
+     private static final String USERNAME = ActiveMQConnection.DEFAULT_USER；
+     // ActiveMQ 的默认登录密码
+     private static final String PASSWORD = ActiveMQConnection.DEFAULT_PASSWORD;
+     // ActiveMQ 的链接地址
+     private static final String BROKEN_URL = ActiveMQConnection.DEFAULT_BROKEN_URL;
+     // 链接工厂
+     ConnectionFatory connectionFactory;
+     // 链接对象
+     Connection connection;
+     // 事务管理
+     Session session;
+     
+     public void init(){
+        try{
+           // 创建一个链接工厂
+           connectionFactory = new ActiveMQConnectionFactory(USERNAME,PASSWORD,BROKEN_URL);
+           // 从工厂中创建一个链接
+           connection = connectionFactory.createConnection();
+           //  开启链接
+           connection.start();
+           //  创建一个事务，这里通过参数可以设置事务的级别
+           session = connection.createSession(false,Session.CLIENT_ACKNOWLEDGE);
+        } catch (JMSException e){
+           e.printStackTrace();
+        }
+     }
+     
+     public void sendMessage(String disname){
+        try{  
+            Queue queue = session.createQueue(disname);
+            MessageProducer messageProducer = session.createProducer(queue);
+            messageProducer.setDeliverMode(DeliveryMode.PRESISTENT);
+            for(int i=0; i<10; i++){
+                Student stu = new Student(i+"");
+                ObjectMessage msg = session.createObjectMessage(stu);
+                System.out.println(stu.getName());
+                messageProducer.send(msg);
+            }
+            // session.commit();  当为Session.AUTO_ACKNOWLEDGE的时候， 需要代码提交
+            connection.close();
+        } catch (JMSException e){
+            e.printStackTrace();
+        }    
+     }
+}
