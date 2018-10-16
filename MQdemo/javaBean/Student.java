44db512d717cbf377e1ffdbd4a11b42a6a56b270
lyxshike <543421638@qq.com>
2018-10-16 12:42:55 +0800

    javabean student

---
+++ MQdemo/javaBean/Student.java
@@ -0,0 +1,22 @@
+public class Student implements Serializable{
+     private String name;
+     
+     public String getName(){
+          return name;
+     }
+     
+     public void setName(String name){
+        this.name = name;
+     }
+     
+     public Student(String name){
+        super();
+        this.name = name;
+     }
+     
+     public Student(){}
+     
+     public String toString(){
+         return "Student[name="+name+"]";
+     }
+}
